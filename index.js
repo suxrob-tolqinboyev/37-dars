24b384c21a7b8222e110a45395994c7d9a00caaf
suxrob-tolqinboyev <tolqinboyevsuxrob053@gmail.com>
2024-12-05 00:30:13 +0500

    37-dars

---
+++ index.js
@@ -0,0 +1,182 @@
+// BIRINCHI VAZIFA
+
+
+// begin 1
+
+// let a = +prompt("a sonini kiriting");
+
+// let P = 4 * a;
+
+// alert("P = " + P);
+
+
+
+// begin 2
+
+// let a = +prompt("A sonini kiriting");
+
+// let S = Math.pow(a, 2);
+
+// alert("S = " + S);
+
+
+
+
+// Begin 3
+// let a = +prompt("A sonini kiriting");
+// let b = +prompt("B sonini kiriting");
+// let S = a * b;
+// let P = 2 * (a + b);
+// alert("S = " + S);
+// alert("P = " + P);
+
+
+// Begin 4
+// let d = +prompt("D sonini kiriting")
+// let L = Math.PI * d;
+// alert("L = " + L);
+
+
+// Begin 5
+// let a = +prompt("A sonini kiriting");
+// let V = Math.pow(a, 3);
+// let S = 6 * Math.pow(a, 2);
+// alert("V = " + V);
+// alert("S = " + S);
+
+
+
+//  Begin 6
+// let a = +prompt("A sonini kiriting");
+// let b = +prompt("B sonini kiriting");
+// let c = +prompt("C sonini kiriting");
+// let V = a * b * c;
+// let S = 2 * (a * b + b * c + a * c);
+// alert("V = " + V);
+// alert("S = " + S);
+
+
+
+
+// Begin 7
+// let r = +prompt("R sonini kiriting");
+// let L = 2 * Math.PI * r;
+// let S = Math.PI * Math.pow(r, 2);
+// alert("L =" + L);
+// alert("S =" + S);
+
+
+
+// Begin 8
+// let a = +prompt("A sonini kiriting");
+// let b = +prompt("B sonini kiriting");
+// let C = (a + b) / 2;
+// alert("C =" + C);
+
+
+
+// Begin 13
+// let R1 = +prompt("R1 sonini kiriting");
+// let R2 = +prompt("R2 sonini kiriting");
+// let S1 =Math.PI * R1;
+// let S2 =Math.PI * R2;
+// let S3 =Math.PI * (R1 - R2);
+// alert("S1 =" + S1);
+// alert("S2 =" + S2);
+// alert("S3 =" + S3);
+
+
+
+// Begin 32
+// let tf = +prompt("tf sonini kiriting");
+// let TC = (tf - 32) * 5 / 9;
+// alert("TC =" + TC);
+
+
+// IKKINCHI VAZUFA
+
+// INTEJIR 1
+// let L = +prompt("L sonini kiriting");
+// let M = L / 100;
+// alert("M =" + M);
+
+
+// INTEJIR 2
+// let M = +prompt("M sonini kiriting");
+// let T = M / 1000;
+// alert("T =" + T);
+
+
+// INTEJIR 3
+// let B = +prompt("B sonini kiriting");
+// let KB = B / 1024;
+// alert("KB =" + KB);
+
+// INTEJIR 4
+// let a = +prompt("A sonini kiriting");
+// let b = +prompt("B sonini kiriting");
+// let C = a / b;
+// alert("C =" + C);
+
+// INTAJIR 5
+// let a = +prompt("A sonini kiriting");
+// let b = +prompt("B sonini kiriting");
+// let C = a / b;
+// alert("C =" + C);
+
+// INTAJIR 6
+// let a = +prompt("A sonini kiriting");
+// let on = Math.floor(a / 10);
+// let bir = a % 10;
+// alert("onliklar =" + on);
+// alert("birliklar =" + bir);
+
+// INTEJIR 7
+// let a = +prompt("A sonini kiriting");
+// let on = Math.floor(a / 10);
+// let bir = a % 10;
+// let Y = on + bir;
+// alert("Yigindi Y =" + Y);
+
+// INTEJIR 8
+// let a = +prompt("A sonini kiriting");
+// let on = Math.floor(a / 10);
+// let bir = a % 10;
+// let al =bir * 10 + on;
+// alert("al =" + al);
+
+// INTEJIR 9
+// let a = +prompt("Uchxonali son kiriting");
+// let yuz = Math.floor(a / 100);
+// alert("yuz =" + yuz);
+
+// INTEJIR 10
+// let a = +prompt("Uchxonalik son kiriting");
+// let bir = a % 10;
+// let on = Math.floor(a / 10) % 10;
+// alert("bir =" +bir);
+// alert("on =" +on);
+
+// INTEJIR 11
+// let a = +prompt("Uchxonalik son kiriting");
+// let bir = a % 10;
+// let on = Math.floor(a / 10) % 10;
+// let yuz = Math.floor(a / 100);
+// let yeg = bir + on + yuz;
+// alert("yeg =" + yeg);
+
+// INTEJIR 12
+// let a = +prompt("Uchxonalik son kiriting");
+// let bir = a % 10;
+// let on = Math.floor(a / 10) % 10;
+// let yuz = Math.floor(a / 100);
+// let yeg = (bir * 100) + (on * 10) + yuz;
+// alert("yeg =" + yeg);
+
+// INTEJIR 13
+// let a = +prompt("Uchxonalik son kiriting");
+// let bir = a % 10;
+// let on = Math.floor(a / 10) % 10;
+// let yuz = Math.floor(a / 100);
+// let yeg = (on * 100) + (bir * 10) + yuz;
+// alert("yeg =" + yeg);
